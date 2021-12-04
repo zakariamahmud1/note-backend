37acbb37df89d60c02624ba197494898364fdc60
unknown <zakaria.mahmud@failureline.com>
2021-12-04 20:22:00 +0100

    Added build and static file option

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 
 const express= require('express')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
 
 
-const cors = require('cors')
+
 
 app.use(cors())
+app.use(express.static('build'))
 
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
